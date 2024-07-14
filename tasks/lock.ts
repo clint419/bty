b443eb8b55230806143cee8ed4d445267cb99280
clint <clint@zklink.org>
2024-07-14 16:39:50 +0800

    Fixed task

--- tasks/lock.ts
+++ tasks/lock.ts
@@ -23,28 +23,28 @@ function distance(past: number, future: number): string {
   return `${days} day(s), ${hours} hour(s), ${minutes} minute(s) and ${seconds} second(s)`;
 }
 
-task("task:withdraw", "Calls the withdraw function of Lock Contract")
-  .addOptionalParam("address", "Optionally specify the Lock address to withdraw")
-  .addParam("account", "Specify which account [0, 9]")
-  .setAction(async function (taskArguments: TaskArguments, hre) {
-    const { ethers, deployments } = hre;
-
-    const Lock = taskArguments.address ? { address: taskArguments.address } : await deployments.get("Lock");
-
-    const signers = await ethers.getSigners();
-    console.log(taskArguments.address);
-
-    const lock = await ethers.getContractAt("Lock", Lock.address);
-
-    const initialBalance = await ethers.provider.getBalance(Lock.address);
-    await lock.connect(signers[taskArguments.account]).withdraw();
-    const finalBalance = await ethers.provider.getBalance(Lock.address);
-
-    console.log("Contract balance before withdraw", ethers.formatEther(initialBalance));
-    console.log("Contract balance after withdraw", ethers.formatEther(finalBalance));
-
-    console.log("Lock Withdraw Success");
-  });
+// task("task:withdraw", "Calls the withdraw function of Lock Contract")
+//   .addOptionalParam("address", "Optionally specify the Lock address to withdraw")
+//   .addParam("account", "Specify which account [0, 9]")
+//   .setAction(async function (taskArguments: TaskArguments, hre) {
+//     const { ethers, deployments } = hre;
+//
+//     const Lock = taskArguments.address ? { address: taskArguments.address } : await deployments.get("Lock");
+//
+//     const signers = await ethers.getSigners();
+//     console.log(taskArguments.address);
+//
+//     const lock = await ethers.getContractAt("Lock", Lock.address);
+//
+//     const initialBalance = await ethers.provider.getBalance(Lock.address);
+//     await lock.connect(signers[taskArguments.account]).withdraw();
+//     const finalBalance = await ethers.provider.getBalance(Lock.address);
+//
+//     console.log("Contract balance before withdraw", ethers.formatEther(initialBalance));
+//     console.log("Contract balance after withdraw", ethers.formatEther(finalBalance));
+//
+//     console.log("Lock Withdraw Success");
+//   });
 
 task("task:deployLock", "Deploys Lock Contract")
   .addParam("unlock", "When to unlock funds in seconds (number of seconds into the futrue)")
